7fb8a8ee75ab117a1d0f71de0d6795c3af8cd8e9
Memmie Lenglet <memmie@lenglet.name>
2017-07-10 23:39:31 +0200

    Add torrentz.unblocked.srl

--- torrentz-magnet.user.js
+++ torrentz-magnet.user.js
@@ -4,6 +4,7 @@
 // @author		mems <memmie@lenglet.name>
 // @homepageURL 	https://github.com/mems/torrentz-magnet
 // @description		Add magnet link to torrentz download page.
+// @match		*://torrentz.unblocked.srl/*
 // @match		*://*.torrentz2.eu/*
 // @match		*://*.torrentz.com/*
 // @match		*://*.torrentz.eu/*
@@ -18,7 +19,7 @@
 // @match		*://*.torrentsmirror.com/*
 // @match		*://*.torrentzeu.to/*
 // @updateURL   	https://openuserjs.org/install/mems/Torrentz_magnet.user.js
-// @version		1.1.5
+// @version		1.1.6
 // @grant		none
 // ==/UserScript==
 
@@ -49,10 +50,11 @@ if(list){
 	let trackers = Array.from(document.querySelectorAll(".trackers dl a")).map(node => node.textContent.trim()).concat(defaultTrackers);
 	let trackersCmps = trackers.reduce((result, uri) => result + "&tr=" + encodeURIComponent(uri), "");
 	let uri = `magnet:?xt=urn:btih:${hash}&dn=${encodeURIComponent(name)}${trackersCmps}`;
+	let v2 = /(^|\.)(torrentz2\.eu|torrentz\.unblocked\.srl)$/.test(location.hostname);
 	let magnetIcon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAYAAAAfSC3RAAAAZUlEQVQokc2RQQrAMAgE9+g/99/5yvaSWDUV0lMrDATMiIsAoIZVbV9jjEQVu/4HohZmAiCSLs63ZOa0Ivb6nThv9D5jkCNRcpHkJqqsXyGpe5VDMUVZ8uOAnCvl9yKpOCR+rsIFGyA1+Hqs6JoAAAAASUVORK5CYII=";// from https://commons.wikimedia.org/wiki/File:Magnet-icon.gif
 	list.querySelector("dl:first-of-type").insertAdjacentHTML("beforebegin", `
 		<dl>
-			<dt><a href="${uri}"><span class="${/(^|\.)torrentz2.eu$/.test(location.hostname) ? `j z s197"></span><span class="u"` : `u" style="background: transparent url('${magnetIcon}') no-repeat 5px center; color: red;"`}>Magnet</span> <span class="n">${name}</span></a></dt>
+			<dt><a href="${uri}"><span class="${v2 ? `j z s197"></span><span class="u"` : `u" style="background: transparent url('${magnetIcon}') no-repeat 5px center; color: red;"`}>Magnet</span> <span class="n">${name}</span></a></dt>
 			<dd>Magnet</dd>
 		</dl>
 	`);
